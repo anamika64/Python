c1f32a26c41ceb2333b093d98026f41292f581b2
anamika64 <81364136+anamika64@users.noreply.github.com>
2021-04-13 14:34:54 +0530

    Add files via upload

---
+++ areaoftrianle.py
@@ -0,0 +1,22 @@
+                            
+#area of traingle
+
+
+
+b=int(input("Enter a base of triangle: "))
+
+
+
+h=int(input("Enter a height of triangle: "))
+
+
+
+x=lambda b,h:1/2*b*h
+
+
+
+z=x(b,h)
+
+
+
+print("Area of triangle: ",z)
