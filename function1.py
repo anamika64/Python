79033bbda24e55a1a103b24508bd981ef1c643ee
anamika64 <81364136+anamika64@users.noreply.github.com>
2021-04-13 14:27:53 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,60 @@
+def add():
+    x=int(input("Enter num1=> "))
+    y=int(input("Enter num2=> "))
+
+    z=x+y
+    print("sum is => ", z)
+
+def sub ():
+    x=int(input("Enter num1=> "))
+    y=int(input("Enter num2=> "))
+
+    z=x-y
+    print("sum is => ", z)
+
+def mul():
+    x=int(input("Enter num1=> "))
+    y=int(input("Enter num2=> "))
+
+    z=x*y
+    print("mul is => ", z)
+
+
+def div():
+    x=int(input("Enter num1=> "))
+    y=int(input("Enter num2=> "))
+
+    z=x/y
+    print("div is => ", z)
+
+def fdiv():
+    x=int(input("Enter num1=> "))
+    y=int(input("Enter num2=> "))
+
+    z=x//y
+    print("fdiv is => ", z)
+
+def mod():
+    x=int(input("Enter num1=> "))
+    y=int(input("Enter num2=> "))
+
+    z=x%y
+    print("mod is => ", z)
+
+def exp():
+    x=int(input("Enter num1=> "))
+    y=int(input("Enter num2=> "))
+
+    z=x**y
+    print("exp is => ", z)
+
+
+    
+
+
+
+
+
+
+
+
