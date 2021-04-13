b5cc15b38a7a505b9d4cab70bb9d9da89285bd06
anamika64 <81364136+anamika64@users.noreply.github.com>
2021-04-13 15:07:45 +0530

    Add files via upload

---
+++ flask1.py
@@ -0,0 +1,15 @@
+from flask import Flask  #flask inbuilt package and Flask inbuilt class
+#step 1
+app=Flask(__name__)  #name_  inbuilt attributes of class
+#app user defined object name
+
+#create decorator function  means to run automatic in browser means decorator function
+#automatic call
+#@
+@app.route("/") #function decorator , its automatic call in browser
+def hello(): #hello user defined function
+    return "Welcome to India "
+
+#main program
+
+app.run()  #run() inbuilt function
