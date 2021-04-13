f8fa0be330304ea047f274a1c56ca84d27308d2d
anamika64 <81364136+anamika64@users.noreply.github.com>
2021-04-13 14:36:33 +0530

    Add files via upload

---
+++ isalpha.py
@@ -0,0 +1,10 @@
+while(True):
+    password=input("Enter password:")
+    if(password.isalpha()):
+        print("valid")
+        break
+    else:
+        print("not a valid,please renter the pssword (plz enter only alpha)")
+
+
+
